3ab2962222928b57b9394e43b8cdacbcfd6f4a9e
jcshim <jcshim@andong.ac.kr>
2019-10-09 13:30:45 +0900

    Added Size

--- Example.cpp
+++ Example.cpp
@@ -2,14 +2,14 @@
 class CMyFrame : public CFrameWnd {
 public:
 	CMyFrame() {
-		Create(NULL, L"ANU Happy!");
+		Create(NULL, L"ANU Happy!", WS_SYSMENU, CRect(90, 120,
+			550, 480));
 	}
 };
 class CExample : public CWinApp {
 	BOOL InitInstance() {
 		CMyFrame* Frame = new CMyFrame();
 		m_pMainWnd = Frame;
-
 		Frame->ShowWindow(SW_NORMAL);
 		Frame->UpdateWindow();
 
